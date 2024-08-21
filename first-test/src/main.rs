46732a1a1ff129b19d151538b8b8241f14138723
Furover <suspicious.porcupine379@passinbox.com>
2024-08-21 11:51:17 -0300

    matches and stuff

--- first-test/src/main.rs
+++ first-test/src/main.rs
@@ -5,7 +5,15 @@ fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 
-fn main() {
+fn shadowing() {
+    //this is shadowing a variable
+    let x: u8 = 5;
+    println!("Variable x1: {}", x);
+    let x: u8 = 10;
+    println!("Variable x2: {}", x);
+}
+
+fn basics() {
     //u is unsigned, which means it can only be positive
     //i is signed, which means it can be negative
     //therefore, i8 can go up to 127 and -127
@@ -29,7 +37,7 @@ fn main() {
         std::mem::size_of_val(&b)
     );
     //constants are always immutable (therefore don't allocate memory) and have a static lifetime (they exist while the program is running)
-    const PI:u8 = 3;
+    const PI: u8 = 3;
     println!(
         "Variable PI: {}, with a size of {} bytes",
         PI,
@@ -44,12 +52,54 @@ fn main() {
             std::mem::size_of_val(&NAME)
         );
     }
+}
 
-    //this is shadowing a variable
-    let x:u8 = 5;
-    println!("Variable x1: {}", x);
-    let x:u8 = 10;
-    println!("Variable x2: {}", x);
+fn conditional() {
+    let age: u8 = 18;
+    //sadly, this is the equivalent to age >= 18 ? "adult" : "child" in rust
+    let condition = if age >= 18 { "adult" } else { "child" };
+    println!("You are {}", condition);
 
+    let i = "hello";
+    let matching = match i {
+        "hello" => "test",
+        &_ => "welp"
+    };
+    println!("The meaning of {i} is to {matching}");
+}
+
+fn loops() {
+    //while example
+    let mut i = 0;
+    while i < 5 {
+        println!("i = {}", i);
+        i += 1;
+    }
+
+    //loop example
+    let mut j = 0;
+    loop {
+        println!("j = {}", j);
+        j += 1;
+        if j > 5 {
+            break;
+        }
+    }
+
+    //for example
+    for k in 0..5 {
+        println!("k = {}", k);
+    }
+    //or
+    for k in 0..=4 {
+        println!("k = {}", k);
+    }
+}
+
+fn main() {
+    basics();
+    shadowing();
     println!("Sum result: {}", sum(5, 10));
+    conditional();
+    loops();
 }
